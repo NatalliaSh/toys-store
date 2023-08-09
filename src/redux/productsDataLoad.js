d6ea64fc9fbbd588a6b14bbb90122d64d65cd207
Natallia Shliapik <nshliapik@gmail,com>
2023-08-09 19:43:41 +0300

    reduce productsDataLoad

--- src/redux/productsDataLoad.js
+++ src/redux/productsDataLoad.js
@@ -1,28 +1,15 @@
-import {
-  updateLoadState,
-  updateDataAll,
-  updateDataByCategory,
-} from './productsSlice.js';
+import { updateLoadState, updateData } from './productsSlice.js';
 import { productsFetchURL } from '../CONST.js';
 
-export async function productsDataLoad(dispatch, category) {
+export async function productsDataLoad(dispatch, category = 'all') {
   try {
     dispatch(updateLoadState({ state: 1, error: null }));
-    let response;
-    if (!category || category === 'all') {
-      response = await fetch(productsFetchURL.all);
-    } else {
-      response = await fetch(productsFetchURL[category]);
-    }
+    const response = await fetch(productsFetchURL[category]);
 
     if (response.ok) {
       const data = await response.json();
+      dispatch(updateData({ category: category, data: data }));
       dispatch(updateLoadState({ state: 2, error: null }));
-      if (!category || category === 'all') {
-        dispatch(updateDataAll(data));
-      } else {
-        dispatch(updateDataByCategory({ category: category, data: data }));
-      }
     } else {
       dispatch(
         updateLoadState({ state: 3, error: 'HTTP error ' + response.status }),
