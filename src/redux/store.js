642983de187a0a37c1d343623b2fe838c4076a33
Natallia Shliapik <nshliapik@gmail,com>
2023-08-12 01:09:52 +0300

    remove paging slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import productsReducer from './productsSlice';
-import pagingReducer from './pagingSlice';
 import basketReducer from './basketSlice';
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
-    paging: pagingReducer,
     basket: basketReducer,
   },
 });
