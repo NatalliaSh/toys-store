00f43cc3e99d0990fa4625b09b725a98b0f06c0a
Natallia Shliapik <nshliapik@gmail,com>
2023-08-11 16:22:28 +0300

    make getdata to productFullCard

---
+++ src/components/cards/ProductCardFull.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { BuyButton } from '../buttons/BuyButton';
+
+import './ProductCardShort.scss';
+
+export const ProductCardFull = ({ productData }) => {
+  return (
+    <div className='ProductCardShort'>
+      <div className='ProductCardShort__image'>
+        <NavLink to={'/product/' + productData.id}>
+          <img src={'/image/products/' + productData.main_image}></img>
+        </NavLink>
+      </div>
+      <div className='ProductCardShort__description'>
+        <NavLink to={'/product/' + productData.id}>
+          <h6>{productData.title}</h6>
+        </NavLink>
+        <BuyButton productData={productData}></BuyButton>
+      </div>
+    </div>
+  );
+};
