42fa3a2bc0bf9e7a246ae18aae50f7843b4436e8
Natallia Shliapik <nshliapik@gmail,com>
2023-08-15 18:25:58 +0300

    config

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyD25BZcyp9Gztr-0CIqmHotyBR1Z0s_KjM',
+  authDomain: 'toystore-86001.firebaseapp.com',
+  projectId: 'toystore-86001',
+  storageBucket: 'toystore-86001.appspot.com',
+  messagingSenderId: '961924735847',
+  appId: '1:961924735847:web:b7974ffcb86fb2272ac5c2',
 };
 
 export function initFireBase() {
